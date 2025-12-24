1b8c32091b2e46acc19eb712e7281a426e4cff74
陈小造 <193269219@qq.com>
2025-12-24 15:28:06 +0800

    feat: 添加 Vant UI 组件库及自动导入功能
    添加 Vant UI 组件库并配置自动导入插件，优化项目结构
    在 App.vue 中添加 Vant 按钮组件示例
    更新相关依赖和配置文件

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
 </template>
 
 <style scoped></style>
