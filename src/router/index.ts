d06d428ad4ef1566fbe6b4cd5ce80e7f5de5126c
chenxiaozao <193269219@qq.com>
2025-12-27 19:14:11 +0800

    Change router history mode to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { getStorageToken } from '@/utils/storage'
 import ArticleView from '@/views/ArticleView.vue'
 import LayoutView from '@/views/LayoutView.vue'
@@ -11,7 +11,7 @@ import UserView from '@/views/tabbar/UserView.vue'
 
 const whiteList = ['/login', '/register']
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
