df0bfca91cd8815130fddbc14be3ea17c4596419
陈小造 <193269219@qq.com>
2025-12-27 14:18:08 +0800

    配置一级路由

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-
-  <van-button type="primary">主要按钮</van-button>
-  <van-button type="success">成功按钮</van-button>
-  <van-button type="default">默认按钮</van-button>
-  <van-button type="danger">危险按钮</van-button>
-  <van-button type="warning">警告按钮</van-button>
+  <!-- 路由视图 -->
+  <router-view />
 </template>
 
 <style scoped></style>
